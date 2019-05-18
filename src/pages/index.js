ee284e83c2f7a0931bfeaa92a8ea8e74ba8be707
xotonic <xotonic@yandex.ru>
2019-05-18 10:31:42 +0200

    Test changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,9 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Dmitriy Kuzmin (@xotonic)" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Please, hire me 🙏</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
