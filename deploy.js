bc4bd396a0080008d0e8d6777ad95d1349c064cf
xotonic <xotonic@yandex.ru>
2019-05-18 15:12:12 +0200

    Fix typo

--- deploy.js
+++ deploy.js
@@ -3,5 +3,5 @@ gh = require("gh-pages")
 
 gh.publish("public", {
     branch: "master",
-    repo : "https://github.com/xotonic/xotonic.gihub.com.git"
+    repo : "https://github.com/xotonic/xotonic.github.com.git"
 }, function(err) { console.log(err)}) 
