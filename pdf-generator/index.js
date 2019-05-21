c4dbe0fcc208b734dc573817af1f92c60d79c395
xotonic <xotonic@yandex.ru>
2019-05-21 08:02:38 +0200

    Move PDF generator in separate package, use react-pdf instead of pdfkit

---
+++ pdf-generator/index.js
@@ -0,0 +1,41 @@
+
+const yaml = require('js-yaml')
+const fs = require('fs')
+import React from 'react';
+import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import ReactPDF from '@react-pdf/renderer';
+
+var yml = yaml.safeLoad(fs.readFileSync('../src/data/test.yml','utf8'))
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'column',
+    backgroundColor: '#FFFFFF'
+  },
+  section: {
+    margin: 5,
+    padding: 5,
+    flexGrow: 0
+  }
+});
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Dmitriy Kuzmin</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Software Engineer</Text>
+      </View>
+    </Page>
+  </Document>
+);
+
+if (!fs.existsSync('static')){
+    fs.mkdirSync('static');
+}
+
+ReactPDF.render(<MyDocument/>, `../static/resume.pdf`);
