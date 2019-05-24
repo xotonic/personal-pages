e455dbcce57ac939a35da14e146192d3f136b722
xotonic <xotonic@yandex.ru>
2019-05-24 13:47:16 +0200

    Reformat Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -27,8 +27,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
@@ -37,15 +37,15 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `./src/data/`,
-      },
+        path: `./src/data/`
+      }
     },
     `gatsby-plugin-sass`,
-  {
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-140823183-1",
-      },
-    },
-  ],
+        trackingId: 'UA-140823183-1'
+      }
+    }
+  ]
 }
