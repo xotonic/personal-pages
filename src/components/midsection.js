b77ae1781f461e4142ca923e6e3a441d86c2901c
xotonic <xotonic@yandex.ru>
2019-05-26 17:59:29 +0200

    Display about contents from yaml

--- src/components/midsection.js
+++ src/components/midsection.js
@@ -1,26 +1,37 @@
 import React from 'react'
 import './style.scss'
 import Timeline from './timeline'
+import { StaticQuery, graphql } from 'gatsby'
 
 const Midsection = () => (
-  <div>
-    <section className="section">
-      <div className="container">
-        <div className="content is-medium">
-          <h2>About me</h2>
-          <p>
-            Software engineer with 2+ years of work experience.
-            Developing solutions in various fields from telecommunications
-            management and marketing to cutting-edge researches of blockchain technology.
-            Constantly investing in education and learning new tools for making better products.
-            Seeking for further gaining and sharing experience as a software developer.
-          </p>
-        </div>
-        <Timeline/>
-        <p>Work on this page is still in progress...</p>
+  <StaticQuery
+    query={graphql`
+        query {
+          allDataYaml {
+            nodes {
+              resume {
+                about
+              }
+            }
+          }
+        }
+    `}
+
+    render={ data =>
+      <div>
+        <section className="section">
+          <div className="container">
+            <div className="content is-medium">
+              <h2>About</h2>
+              <p>{ data.allDataYaml.nodes[0].resume.about }</p>
+            </div>
+            <Timeline/>
+            <p>Work on this page is still in progress...</p>
+          </div>
+        </section>
       </div>
-    </section>
-  </div>
+    }
+  />
 )
 
 export default Midsection
