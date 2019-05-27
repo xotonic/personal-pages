f15ab6aa9d11dec5facbc753c97f5b9446752d8c
xotonic <xotonic@yandex.ru>
2019-05-27 03:37:10 +0200

    Fix hero size and central title overflow

--- src/components/header.js
+++ src/components/header.js
@@ -4,13 +4,13 @@ import './style.scss'
 import Navbar from './navbar'
 
 const Header = ({ siteTitle }) => (
-  <section className="hero gradientBg is-fullheight-with-navbar">
+  <section className="hero gradientBg is-fullheight">
     <Navbar />
     <div className="hero-body">
       <div className="container center">
         <article className="media">
           <div className="media-content">
-            <div className="content">
+            <div className="content no-scroll">
               <h1 className="is-uppercase is-size-1 has-text-white">
                                 Dmitriy Kuzmin
               </h1>
